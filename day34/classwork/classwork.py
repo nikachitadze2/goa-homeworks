d92cbb5a763296ded6ae1f4faa8a45f8d89bc79f
nikachitadze2 <nikolozichitadze107>
2025-10-09 23:20:34 +0400

    fiveth commit

---
+++ day34/classwork/classwork.py
@@ -0,0 +1,16 @@
+students= [ "nika",  "nika", "gio", "luka"]
+students.append("vano")
+students.pop("luka")
+print(students.count("nika"))
+
+students= [ "nika","gio", "luka"]
+students.clear()
+print(students)
+
+
+
+
+fruit = {"banana", "apple"}
+vagetebels = {"tomato", "potato"}
+set3 = fruit.union(vagetebels)
+print(set3)
