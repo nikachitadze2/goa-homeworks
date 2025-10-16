f8ee31f8c34acf66fddb7727bcc43dc5e5f6982b
nikachitadze2 <nikolozichitadze107>
2025-10-16 19:48:24 +0400

    fiveth commit

--- day34/classwork/classwork.py
+++ day34/classwork/classwork.py
@@ -3,14 +3,22 @@ students.append("vano")
 students.pop("luka")
 print(students.count("nika"))
 
+# list ინდექსი არის ერთ ერთი კოლექციის და მონაცემთა ტიპი რომელიც შეცვლადია , იდექსები გააჩნია
+
 students= [ "nika","gio", "luka"]
 students.clear()
 print(students)
 
-
+# tuple არის დალაგებული მარა შეუცვლელი კოლექცაია
 
 
 fruit = {"banana", "apple"}
 vagetebels = {"tomato", "potato"}
 set3 = fruit.union(vagetebels)
 print(set3)
+
+#set არის დაულაგებელი კოლექცია რომელშიც ინახება მხოლოდ უნიკალური ელემენტი , ორი ერთნაირი არ შეიძლება
+
+
+
+
